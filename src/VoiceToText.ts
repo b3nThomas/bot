d15be1391d594cfde9dd2594959c4b5a475b0d11
b3nThomas <b3nadthomas@gmail.com>
2019-08-26 20:06:11 +0100

    Ran into wall. Need nuxt...

--- src/VoiceToText.ts
+++ src/VoiceToText.ts
@@ -1,3 +1,23 @@
+import { createReadStream } from 'fs';
+import SpeechToTextV1 from 'ibm-watson/speech-to-text/v1';
+
+const options = {
+    iam_apikey: '',
+    url: 'h'
+};
+
+const speechToText = new SpeechToTextV1(options);
+
 export const voiceToTextCallback = audioFile => {
-    console.log(audioFile);
+    const recognizeParams = {
+        audio: createReadStream(audioFile),
+        content_type: 'audio/flac',
+    };
+
+    speechToText.recognize(recognizeParams, (err, res) => {
+        if (err) {
+            return console.error(err);
+        }
+        console.log(JSON.stringify(res, null, 2));
+    });
 };
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const path = require('path');
 module.exports = {
     entry: './main.js',
     mode: 'development',
+    node: {
+        fs: 'empty'
+    },
     output: {
         filename: 'bottom.js',
         path: path.resolve(__dirname, 'public')
